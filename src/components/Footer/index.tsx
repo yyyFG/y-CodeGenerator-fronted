1c1b7bb3b7c73faba0cd7f0490459aaf7604d176
yyyFG <2523766521@qq.com>
2025-09-06 16:59:52 +0800

    first commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+import '@umijs/max';
+import React from 'react';
+
+const Footer: React.FC = () => {
+  const defaultMessage = 'yyy';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      style={{
+        background: 'none',
+      }}
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: (
+            <>
+              <GithubOutlined /> yyy
+            </>
+          ),
+          href: 'https://github.com/yyyFG',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
